55efb65e49828a48a9dea2072b14987c6790405e
SheriffHobo <zombiemaster@gmail.com>
2019-02-11 12:44:15 -0800

    lots of js updates. random number generate on page load

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+$(function () {
+
+// Variables for storing the various numbers
+let totNumber = [];
+let winCount = 0;
+let lossCount = 0;
+
+// On page load, generate target & crystal numbers
+let ranNum1 = Math.floor(Math.random() * (120 - 19)) + 19;
+document.querySelector('#tarnum').innerHTML = ranNum1;
+let blueC = Math.floor(Math.random() * (12 - 1)) + 1;
+// Need to assiciate 'blueC' value with 'bluec' pic
+console.log(blueC);
+let greenC = Math.floor(Math.random() * (12 - 1)) + 1;
+
+let pinkC = Math.floor(Math.random() * (12 - 1)) + 1;
+
+let purpC = Math.floor(Math.random() * (12 - 1)) + 1;
+
+
+// Playing the game
+
+// Update win/lose scores
+
+// Reset page
+
+});
+
